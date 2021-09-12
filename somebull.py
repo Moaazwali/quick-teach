cff23ccdb6f6da17c2006e2431be8ae3e9158066
Moaazwali <90515325+Moaazwali@users.noreply.github.com>
2021-09-12 15:09:04 +0300

    Add files via upload

---
+++ somebull.py
@@ -0,0 +1,43 @@
+import turtle
+
+import time
+
+
+
+turtle.setup(400, 700)
+
+
+turtle.speed(8)
+
+turtle.penup()
+
+turtle.left(90)
+
+turtle.fd(200)
+turtle.pendown()
+turtle.right(90)
+turtle.fillcolor('red')
+turtle.begin_fill()
+
+turtle.circle(100, 1008)
+turtle.end_fill()
+
+turtle.right(90)
+turtle.fd(300)
+
+
+turtle.fillcolor('blue')
+
+
+turtle.begin_fill()
+
+
+turtle.circle(109,100)
+turtle.end_fill
+
+
+
+
+
+
+
